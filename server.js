ddf0fab9633006376027ed62e23535aeca08271b
Theodore Whitbeck <55822504+Vinical@users.noreply.github.com>
2020-06-15 21:41:49 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
-var express = require(‘express’);
-var data = require(‘./data/test.json’);
+var express  = require('express');
+var data = require('./data/test.json');
 
 var app = express();
-const PORT= 3000;
+const PORT = 3000;
 
-//we can now serve static pages 
-app.use (express.static(_dirname));
-//app.listen(3000)
+//we can now serve up static pages
+app.use(express.static(__dirname));
+
+//app.listen(3000);
+
 app.listen(PORT,()=>{
-console.log(`App is running on port ${PORT}`);
-console.log(data);
-
+  console.log(`App is running on port ${PORT}`);
+  console.log(data);
 });
