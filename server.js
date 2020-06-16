a099c7cdb9796f27105930ba7d39fb065658d12b
Theodore Whitbeck <55822504+Vinical@users.noreply.github.com>
2020-06-16 15:20:35 -0700

    Update server.js

--- server.js
+++ server.js
@@ -4,12 +4,23 @@ var data = require('./data/test.json');
 var app = express();
 const PORT = 3000;
 
-//we can now serve up static pages
-app.use(express.static(__dirname));
+app.set('view engine','ejs');//sent views to EJS
 
-//app.listen(3000);
+//this is how a route is built in Express
+app.get('/', (req,res)=>{
+  var title = "Our Home Page";
+  res.render('pages/index',{title:title});
+});
+
+app.get('/users', (req,res)=>{
+  var title = "Our Home Page";
+  res.render('users/index',{
+    title:title,
+    users:data
+  });
+});
 
 app.listen(PORT,()=>{
   console.log(`App is running on port ${PORT}`);
-  console.log(data);
+  //console.log(data);
 });
